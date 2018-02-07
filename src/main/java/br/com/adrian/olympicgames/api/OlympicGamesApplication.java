f39f2325f0019a6dfe5a90fe2a24697cf20aab14
Adrian Marcell <adrian_marcell@live.com>
2018-02-06 22:13:35 -0200

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/br/com/adrian/olympicgames/api/OlympicGamesApplication.java
@@ -0,0 +1,12 @@
+package br.com.adrian.olympicgames.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympicGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlympicGamesApplication.class, args);
+	}
+}
